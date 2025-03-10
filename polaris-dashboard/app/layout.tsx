33bd75b77eeb25ff3e59c719907d809cdf6e14cb
NayunKang <167592528+NayunKang@users.noreply.github.com>
2025-03-09 21:08:18 -0500

    sidebar

--- polaris-dashboard/app/layout.tsx
+++ polaris-dashboard/app/layout.tsx
@@ -3,10 +3,10 @@ import { Metadata, Viewport } from "next";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
-
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import Sidebar from "@/components/sidebar/sidebar"; 
 
 export const metadata: Metadata = {
     title: {
@@ -32,10 +32,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <head />
             <body className={clsx("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
                 <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-                    <div className="relative flex flex-col h-screen">
-                        <Navbar />
-                        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
-                        <footer className="w-full flex items-center justify-center py-3" />
+                    <div className="relative flex h-screen">
+                        <Sidebar /> 
+                        <div className="flex flex-col flex-1">
+                            <Navbar />
+                            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">{children}</main>
+                            <footer className="w-full flex items-center justify-center py-3" />
+                        </div>
                     </div>
                 </Providers>
             </body>
